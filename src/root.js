00330b193849d2dfefc03d96aae246c37540ca4e
Keith Ng <keith@point.org.nz>
2018-07-26 19:50:22 +1200

    New cluster based implementation

--- src/root.js
+++ src/root.js
@@ -3,95 +3,109 @@ import $ from "jquery"
 import * as d3 from "d3"
 import * as d3jetpack from "d3-jetpack"
 
-// Polyfill for fetch/promise - only required if using d3-csv on IE<11
-// import 'whatwg-fetch'
-// import 'promise-polyfill/src/polyfill'
-
 import HTML from "./root.html"
 import "./base.less"
 import "./root.less"
 
-import Vis from "./lib/baseline.js"
-import rawData from "./data/dummy.csv"
+import Beeswarm from "./lib/beeswarm-canvas.js"
+import rawData from "./data/parsed.csv"
 
 
 class Main {
     constructor () {
         // Clean data
         console.log("Raw data:", rawData)
-        let data = this.data = this.cleanData(rawData)
-        data = this.filterData(this.data, {
-            name: ["Budget 2018", "Labour's Plan"],
-            measure: "Health"
-        })
+        const data = this.cleanData(rawData)
         console.log("Cleaned data:", data)
+
+        // Create clusters
+        const POS = _(data).groupBy("outcome")
+                           .map((rows, outcome) => {
+                               return {
+                                   outcome,
+                                   count: rows.length,
+                                   r: rows.length * 0.5
+                               }
+                           }).value()
 
         // Set up visualisation
         $("#root").addClass("nzherald").append(HTML)
-        const V = new Vis({
-            container: "#root .mainvis",
+        const B = new Beeswarm({
+            container: ".beeswarm",
             scale: {
-                x: d3.scalePoint(),
+                x: d3.scaleLinear(),
                 y: d3.scaleLinear(),
-                c: d3.scaleOrdinal(["#2F4285", "#B22A2E", "#7D1A6E"])
-                     .domain(["Pre-Election Forecast", "Labour's Plan", "Budget 2018"])
-            },
-            axis: {
-                x: d3.axisBottom().tickPadding(32).tickSize(0),
-                y: d3.axisLeft().ticks(3).tickPadding(16).tickSize(4)
-            },
-            format: {
-                val: (d) => d3.format("$.3s")(d).replace(/G/, "B")
+                r: d3.scaleOrdinal().range([3]),
+                c: d3.scaleOrdinal().range(d3.schemeCategory10)
             }
         })
-        console.log("Visualisation:", V)
 
-        // Populate visualisation
-        V.setData(data)
-        this.fadeOut()
-    }
-
-    cleanData (rawData) {
-        const years = ["2018/19", "2019/20", "2020/21", "2021/22"]
-        return _.map(rawData, row => {
-            return {
-                name: row.scenario,
-                measure: row.measure,
-                points: _.map(years, y => {
-                    return {period: y, val: row[y] * 1000000}
-                })
-            }
-        })
-    }
-
-    filterData (data, filter) {
-        _.each(filter, (v, k) => {
-            if (v instanceof Array) {
-                data = _.filter(data, d => v.indexOf(d[k]) !== -1)
-            }
-            else {
-                data = _.filter(data, d => d[k] === v)
-            }
-        })
-        return data
-    }
-
-    fadeOut (b) {
-        sessionStorage.setItem("loading", "done");
-        if (typeof($) !== "undefined") {
-            $("#loading").fadeTo(600, 0.01, () => {
-                $("#loading").remove()
-                console.log("Loading screen removed.")
-                if (b) b()
-            })
-        } else {
-            var loadingRemove = document.getElementById("loading")
-            if (loadingRemove) {
-                document.body.removeChild(loadingRemove)
-                console.log("Loading screen removed.")
-            }
-            if (b) b()
+        B.getX = function (d) {
+            let pos = _.find(POS, {outcome: d.outcome})
+            return (pos) ? pos.x || 0 : 0
         }
+        B.getY = function (d) {
+            let pos = _.find(POS, {outcome: d.outcome})
+            return (pos) ? pos.y || 0 : 0
+        }
+        B.getR = function (d) { return 3 }
+        B.getC = function (d) { return this.scale.c(d.decile) }
+
+        // Initialise
+        B.setData(data)
+        this.forceClusters(POS, () => B.setNodes()) // Position clusters
+        $("#loading").fadeTo(600, 0.01, () => $("#loading").remove())
+    }
+
+    // Generate simulated students
+    cleanData (rawData) {
+        let out = []
+        _.each(rawData, d => d.val = _.round(d.val / 50))
+        _(rawData)
+            .filter({year: "2015"})
+            .groupBy("outcome").each((r, outcome) => {
+                // Set up cohort
+                let total = _.find(r, {type: "Total"})             // Each outcome should have one total
+                let cohort = Array(total.val)                      // Generate empty array for cohort
+                cohort = _.map(cohort, d => { return {outcome} })  // Fill with outcome placeholders
+                out = out.concat(cohort)                           // Push cohort
+
+                // Set demographics of cohort
+                _(r).reject({type: "Total"})
+                    .groupBy("type").each((s, type) => {
+                        let pool = cohort.slice(),                 // Reset pool
+                            k = type.toLowerCase()
+                        _.each(s, d => {                           // Iterate through rows
+                            let sample = _.sampleSize(pool, d.val) // Grab a random sample
+                            _.each(sample, e => e[k] = d.subgroup) // Tag sample with subgroup
+                            pool = _.difference(pool, sample)      // Remove sample from pool
+                        })
+                    })
+            })
+        return out
+    }
+
+    // Place nodes using d3-force
+    forceClusters (nodes, onTick) {
+        let opt = {
+            chargeStr: -10,
+            collideStr: 0.6
+        }
+        var width = $("canvas").width(),
+            height = $("canvas").height()
+        _.each(nodes, d => {d.x = width / 2, d.y = height / 2})
+
+        d3.forceSimulation()
+          .force("charge",  d3.forceManyBody().strength(opt.chargeStr))
+          .force("collide", d3.forceCollide().strength(opt.collideStr).radius(d => d.r * 1.2))
+          .nodes(nodes)
+          .on("tick", () => {
+              _.each(nodes, d => {
+                  d.x = _.clamp(d.x, d.r, width - d.r)
+                  d.y = _.clamp(d.y, d.r, height - d.r)
+              })
+              onTick()
+          })
     }
 }
 
