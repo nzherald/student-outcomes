e91d19b40182158883ab92eb5f6bb0726963212c
Keith Ng <keith@point.org.nz>
2018-07-27 15:15:49 +1200

    Force clustering and colour changing

--- src/root.js
+++ src/root.js
@@ -15,18 +15,8 @@ class Main {
     constructor () {
         // Clean data
         console.log("Raw data:", rawData)
-        const data = this.cleanData(rawData)
-        console.log("Cleaned data:", data)
-
-        // Create clusters
-        const POS = _(data).groupBy("outcome")
-                           .map((rows, outcome) => {
-                               return {
-                                   outcome,
-                                   count: rows.length,
-                                   r: rows.length * 0.5
-                               }
-                           }).value()
+        const nodes = this.cleanData(rawData)
+        console.log("Cleaned data:", nodes)
 
         // Set up visualisation
         $("#root").addClass("nzherald").append(HTML)
@@ -35,25 +25,71 @@ class Main {
             scale: {
                 x: d3.scaleLinear(),
                 y: d3.scaleLinear(),
-                r: d3.scaleOrdinal().range([3]),
+                // r: d3.scaleOrdinal().range([3, 16]),
                 c: d3.scaleOrdinal().range(d3.schemeCategory10)
+            },
+            chargeStr: -0.7,
+            collideStr: 0.4,
+            clusterStr: 0.08
+        })
+        B.sim.force("charge").distanceMax(120)
+        B.getX = function (d) { return d.anchor.x }
+        B.getY = function (d) { return d.anchor.y }
+        B.getR = function (d) { return 3 }
+        B.getC = function (d) { return d.cVal }
+
+        // Set controllers
+        $("#centre").on("click", () => {
+            this.toCentre(nodes)
+            B.setNodes()
+            B.sim.alpha(1)
+            B.sim.restart()
+        })
+        $("#cluster").on("click", () => {
+            this.toClusters(nodes, () => B.setNodes())
+            B.sim.alpha(1)
+            B.sim.restart()
+        })
+        $("#ethnicity").on("click", () => {
+            const colours = {
+                "Māori": "#D62728",
+                "European\\Pākehā": "#1F77B4",
+                "Pacific": "#8C564B",
+                "Asian": "#FF7F0E",
+                "MELAA": "#2CA02C",
+                "Other": "#7F7F7F"
             }
+            _(nodes).groupBy("ethnicity").each((v, k) => {
+                let c = colours[k]
+                _.each(v, d => d.cVal = c)
+            })
+            B.setNodes()
+        })
+        $("#deciles").on("click", () => {
+            const colours = {
+                "Decile 1": "#fc8d59",
+                "Decile 2": "#fc8d59",
+                "Decile 3": "#fc8d59",
+                "Decile 4": "#ffffbf",
+                "Decile 5": "#ffffbf",
+                "Decile 6": "#ffffbf",
+                "Decile 7": "#ffffbf",
+                "Decile 8": "#91bfdb",
+                "Decile 9": "##91bfdb",
+                "Decile 10": "#91bfdb",
+                "Not Applicable": "#bfbfbf"
+            }
+            _(nodes).groupBy("decile").each((v, k) => {
+                let c = colours[k]
+                _.each(v, d => d.cVal = c)
+            })
+            B.setNodes()
         })
 
-        B.getX = function (d) {
-            let pos = _.find(POS, {outcome: d.outcome})
-            return (pos) ? pos.x || 0 : 0
-        }
-        B.getY = function (d) {
-            let pos = _.find(POS, {outcome: d.outcome})
-            return (pos) ? pos.y || 0 : 0
-        }
-        B.getR = function (d) { return 3 }
-        B.getC = function (d) { return this.scale.c(d.decile) }
-
         // Initialise
-        B.setData(data)
-        this.forceClusters(POS, () => B.setNodes()) // Position clusters
+        _.each(nodes, d => d.cVal = d3.schemeCategory10[0]) // Base colour
+        this.toCentre(nodes)
+        B.setData(nodes)
         $("#loading").fadeTo(600, 0.01, () => $("#loading").remove())
     }
 
@@ -85,24 +121,52 @@ class Main {
         return out
     }
 
+    // Anchor to centre
+    toCentre (nodes) {
+        let centre = {
+                x: $("canvas").width() / 2,
+                y: $("canvas").height() / 2
+            }
+        _.each(nodes, d => d.anchor = centre)
+    }
+
+    // Anchor to clusters
+    toClusters (nodes, onTick) {
+        const POS = _(nodes).groupBy("outcome")
+                            .map((rows, outcome) => {
+                                return {
+                                    outcome,
+                                    count: rows.length,
+                                    r: rows.length * 0.2
+                                }
+                           }).value()
+        _(nodes).groupBy("outcome").each((rows, outcome) => {
+            let pos = _.find(POS, {outcome})
+            _.each(rows, d => d.anchor = pos)
+        })
+        this.forceClusters(POS, () => {
+            onTick()
+        })
+    }
+
     // Place nodes using d3-force
     forceClusters (nodes, onTick) {
         let opt = {
             chargeStr: -10,
-            collideStr: 0.6
+            collideStr: 0.8
         }
         var width = $("canvas").width(),
             height = $("canvas").height()
-        _.each(nodes, d => {d.x = width / 2, d.y = height / 2})
-
         d3.forceSimulation()
+          .force("center",  d3.forceCenter().x(width / 2).y(height / 2))
           .force("charge",  d3.forceManyBody().strength(opt.chargeStr))
           .force("collide", d3.forceCollide().strength(opt.collideStr).radius(d => d.r * 1.2))
           .nodes(nodes)
           .on("tick", () => {
               _.each(nodes, d => {
-                  d.x = _.clamp(d.x, d.r, width - d.r)
-                  d.y = _.clamp(d.y, d.r, height - d.r)
+                  let padding = d.r * 1.2
+                  d.x = _.clamp(d.x, padding, width - padding)
+                  d.y = _.clamp(d.y, padding, height - padding)
               })
               onTick()
           })
