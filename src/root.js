d1055df2de130cd30aac31b73121b81e676f83cc
Keith Ng <keith@point.org.nz>
2018-09-19 10:11:08 +1200

    Full interactive version

--- src/root.js
+++ src/root.js
@@ -28,50 +28,118 @@ class Main {
         this.legend = this.makeLegend()
 
         // Initialise
-        this.script = this.setScript(nodes)
-        this.script.captureKeystrokes()
         this.toCentre(nodes)
         this.swarm.setData(nodes)
-        this.script.setPos(0)
+        this.swarm.clusterBy("outcome", {
+            edgePadding: 20,
+            chargeStr: -10,
+            collideRad: 1.2,
+            collideStr: 1.2,
+            labels: {
+                offset: 0.6,
+                arcStart: 0.63,
+                arcEnd: -0.6
+            }
+
+        })
+        _.each(nodes, d => d.cVal = d3.schemeCategory10[0]) // Base colour
+        this.legend.$.hide()
+        this.setButtons(nodes)
+
+        let vals = _(data).map().flatten()
+                          .filter(d => {
+                              d.val *= 1
+                              return ["Asian"].indexOf(d.subgroup) > -1
+                          }).value()
+        let sum = _(vals).sumBy("val")
+        _(vals).groupBy("outcome").each((rows, outcome) => {
+            let val = _.sumBy(rows, "val")
+            console.log(outcome, _.round(val / sum, 3))
+        })
+
         $("#intro").trigger("click")
         $("#loading").fadeTo(600, 0.01, () => $("#loading").remove())
     }
 
-    setScript (nodes) {
-        return new ScriptBox({container: "#scriptbox"}, [{
-            name: "intro",
-            html: "<p>Each dot represents 50 students who left school in 2015.</p>" +
-                  "<p><b>Click next to see more.</b></p>",
-            action: () => {
-                this.swarm.clusterBy("outcome", {
-                    edgePadding: 20
-                })
-                _.each(nodes, d => d.cVal = d3.schemeCategory10[0]) // Base colour
-                this.legend.$.hide()
-            }
-        },{
-            name: "decilehigh",
-            html: "<p>For students from the <b>least deprived 30%</b> of schools, nearly 50% entered university.</p>",
-            action: () => {
-                this.setColour(nodes, "Decile", [{
-                    label: "Decile 8-10 (Least deprived)",
-                    vals: ["Decile 8", "Decile 9", "Decile 10"],
-                    colour: "#29A35A"
-                }])
-            }
-        },{
-            name: "decilelow",
-            html: "<p>For students from the <b>most deprived 30%</b> of schools, however, only 17% entered university.</p>",
-            action: () => {
-                this.setColour(nodes, "Decile", [{
-                    label: "Decile 1-3 (Most deprived)",
-                    vals: ["Decile 1", "Decile 2", "Decile 3"],
-                    colour: "#9970AB"
-                }])
-            }
-        }])
-    }
 
+    setButtons (nodes) {
+        let el = d3.selectAll("div.controller").append("div.decile")
+        el.append("div").html("Decile 10 (10% Least deprived)")
+        .on("click", () => {
+            this.setColour(nodes, "Decile", [{
+                label: "Decile 10 (Least deprived)",
+                vals: ["Decile 10"],
+                colour: "#29A35A"
+            }])
+            d3.select("div.text").html("Of students from the <b>least deprived 10%</b> of schools, 57% entered university, and 29% were not enrolled in a tertiary institution.")
+        })
+        el.append("div").html("Decile 1 (10% Most deprived)")
+          .on("click", () => {
+              this.setColour(nodes, "Decile", [{
+                  label: "Decile 1 (Most deprived)",
+                  vals: ["Decile 1"],
+                  colour: "#9970AB"
+              }])
+              d3.select("div.text").html("Of students from the <b>most deprived 10%</b> of schools, 13% entered university, and 49% were not enrolled in a tertiary institution.")
+          })
+
+        el.append("div").html("Decile 8-10 (30% Least deprived)")
+          .on("click", () => {
+              this.setColour(nodes, "Decile", [{
+                  label: "Decile 8-10 (Least deprived)",
+                  vals: ["Decile 8", "Decile 9", "Decile 10"],
+                  colour: "#29A35A"
+              }])
+              d3.select("div.text").html("Of students from the <b>least deprived 30%</b> of schools, 49% entered university, and 32% were not enrolled in a tertiary institution.")
+          })
+        el.append("div").html("Decile 1-3 (30% Most deprived)")
+          .on("click", () => {
+              this.setColour(nodes, "Decile", [{
+                  label: "Decile 1-3 (Most deprived)",
+                  vals: ["Decile 1", "Decile 2", "Decile 3"],
+                  colour: "#9970AB"
+              }])
+              d3.select("div.text").html("Of students from the <b>most deprived 30%</b> of schools, 17% entered university, and 46% were not enrolled in a tertiary institution.")
+          })
+
+        el = d3.selectAll("div.controller").append("div.ethnicity")
+        el.append("div").html("Māori")
+          .on("click", () => {
+              this.setColour(nodes, "Ethnicity", [{
+                  label: "Māori",
+                  vals: ["Māori"],
+                  colour: "#BE3738"
+              }])
+              d3.select("div.text").html("For <b>Māori</b> students, 13% entered university, and 50% were not enrolled in a tertiary institution.")
+          })
+        el.append("div").html("Pākehā")
+          .on("click", () => {
+              this.setColour(nodes, "Ethnicity", [{
+                  label: "Pākehā",
+                  vals: ["European\\Pākehā"],
+                  colour: "#82B7D3"
+              }])
+              d3.select("div.text").html("For <b>Pākehā</b> students, 34% entered university, and 39% were not enrolled in a tertiary institution.")
+          })
+        el.append("div").html("Pacifika")
+          .on("click", () => {
+              this.setColour(nodes, "Ethnicity", [{
+                  label: "Pacifika",
+                  vals: ["Pacific"],
+                  colour: "#BEA037"
+              }])
+              d3.select("div.text").html("For <b>Pacifika</b> students, 22% entered university, and 46% were not enrolled in a tertiary institution.")
+          })
+        el.append("div").html("Asian")
+          .on("click", () => {
+              this.setColour(nodes, "Ethnicity", [{
+                  label: "Asian",
+                  vals: ["Asian"],
+                  colour: "#3ABB92"
+              }])
+              d3.select("div.text").html("For <b>Asian</b> students, 61% entered university, and 22% were not enrolled in a tertiary institution.")
+          })
+    }
 
     //==========//
     //   Data   //
@@ -89,14 +157,14 @@ class Main {
             let total = _.find(d, {type: "Total"})           // Each outcome should have one total
             let cohort = Array(_.ceil(total.val / 50))       // Generate empty array for cohort
             cohort = _.map(cohort, () => {return {outcome}}) // Fill with outcome placeholders
-            _.each(["Decile", "Ethnicity"], type => {        // For each type
+            _(d).groupBy("type").each((rows, type) => {      // For each type
                 let sample = _.shuffle(cohort)               // Generate a new randomised sample
-                let counter = _(d).filter({type}).map(r => { // For each subgroup of this type
+                let counter = _.map(rows, r => {             // For each subgroup of this type
                     let c = {key: r.subgroup, val: r.val}    // Create counter
                     sample.pop()[type] = c.key               // Add one so there's at least one node
                     c.val -= 50                              // Reduce counter
                     return c
-                }).value()
+                })
                 while (sample.length) {                      // Assign the rest of the cohort
                     let c = _.maxBy(counter, "val")          // Start with the highest counter value
                     sample.pop()[type] = c.key               // Assign and remove from sample
